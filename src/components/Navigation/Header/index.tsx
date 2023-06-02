9d839f3d0cdd2c006865085d721e2ce65f1a35d4
Salik Imtiaz <r.salikimtiaz@gmail.com>
2023-06-02 16:09:40 +0400

    dropdown menu added for small screen

--- src/components/Navigation/Header/index.tsx
+++ src/components/Navigation/Header/index.tsx
@@ -1,8 +1,11 @@
+import { Menu, Transition } from '@headlessui/react'
 import DialogBox from 'components/DialogBox'
+import { Feeds, Topics } from 'components/Navigation/Navlinks'
 import ThemeSwitcher from 'components/ThemeSwitcher'
 import LoginForm from 'containers/Forms/Login'
 import SignupForm from 'containers/Forms/Signup'
-import { useState } from 'react'
+import { feedsInterface, topicsInterface } from 'interfaces/menuInterfaces'
+import { Fragment, useState } from 'react'
 import { AiOutlineUser } from 'react-icons/ai'
 import { BsChevronDown, BsFillArrowUpRightCircleFill, BsQrCodeScan, BsReddit, BsSearch } from 'react-icons/bs'
 import { colors } from 'styles/colors'
@@ -43,10 +46,56 @@ function Header() {
           <span className='text-lg xs:hidden lg:inline'>reddit</span>
         </button>
 
-        <div className='xs:inline md:hidden'>
-          <button className='btn btn-sm'>
-            <BsFillArrowUpRightCircleFill /> <BsChevronDown />
-          </button>
+        <div className='lg:hidden'>
+          <Menu as='div' className='relative inline-block text-left'>
+            <Menu.Button className='flex items-center gap-x-4 rounded-lg border border-gray-100 px-4 py-3 dark:border-gray-700'>
+              <BsFillArrowUpRightCircleFill /> <BsChevronDown />
+            </Menu.Button>
+            <Transition
+              as={Fragment}
+              enter='transition ease-out duration-100'
+              enterFrom='transform opacity-0 scale-95'
+              enterTo='transform opacity-100 scale-100'
+              leave='transition ease-in duration-75'
+              leaveFrom='transform opacity-100 scale-100'
+              leaveTo='transform opacity-0 scale-95'
+            >
+              <Menu.Items className='absolute left-0 top-6 mt-2 w-60 origin-top-left overflow-hidden border-x border-b border-gray-100 bg-white  ring-1 ring-black ring-opacity-5 focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:text-white'>
+                <h6 className='px-8 pb-2 pt-6 dark:text-gray-500'>Feeds</h6>
+                {Feeds.map((feed: feedsInterface) => (
+                  <Menu.Item key={feed.label}>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-gray-200 dark:bg-gray-700'
+                            : 'bg-white text-black dark:bg-gray-800 dark:text-white'
+                        } group flex w-full items-center gap-x-4 px-8 py-2 text-sm`}
+                      >
+                        <span className='text-xl'>{feed.icon}</span> {feed.label}
+                      </button>
+                    )}
+                  </Menu.Item>
+                ))}
+                <h6 className='px-8 pb-2 pt-6 dark:text-gray-500'>Topics</h6>
+                {Topics.map((topic: topicsInterface) => (
+                  <Menu.Item key={topic.label}>
+                    {({ active }) => (
+                      <button
+                        className={`${
+                          active
+                            ? 'bg-gray-200 dark:bg-gray-700'
+                            : 'bg-white text-black dark:bg-gray-800 dark:text-white'
+                        } group flex w-full items-center gap-x-4 px-8 py-2 text-sm`}
+                      >
+                        <span className='text-xl'>{topic.icon}</span> {topic.label}
+                      </button>
+                    )}
+                  </Menu.Item>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </div>
 
         <div
